14fb191c296a52200fa29a81949d81caa9053008
Alex Blinov <alex@alexout.com>
2023-06-19 20:42:27 -0700

    implemented notification of users from settings

--- src/check.ts
+++ src/check.ts
@@ -1,96 +1,82 @@
 import axios from 'axios';
 import moment from 'moment';
 import { Telegraf } from 'telegraf';
+import { getConnectedClient } from './db';
+import { getLocationsQuery, getUsersWithLaterAppointmentQuery, IUser, ILocation } from './queries';
+import { getEnvVariableValueOrExit } from './helpers';
 
-const token: string = process.env.BOT_TOKEN ?? exitWhenEnvVariableNotDefined('BOT_TOKEN');
-const chatID: number = parseInt(process.env.CHAT_ID ?? exitWhenEnvVariableNotDefined('CHAT_ID'), 10);
-
+const token: string = getEnvVariableValueOrExit('BOT_TOKEN');
 const bot = new Telegraf(token);
 
-function exitWhenEnvVariableNotDefined(variableName: string): never {
-    console.error(`Environment variable ${variableName} is not defined.`);
-    process.exit(2);
+interface Appointment {
+    startTimestamp: string;
+    active: boolean;
+    [key: string]: any; // Optional: Allow any additional properties
+  }
+
+async function getDates(locationId: string): Promise<moment.Moment[]> {
+  const response = await axios.get(`https://api.example.com/locations/${locationId}/dates`);
+  const appointments: Appointment = response.data
+  return appointments
+            .filter(appointment => appointment.active)
+            .map(appointment => moment(appointment.startTimestamp, 'YYYY-MM-DDTHH:mm'));
 }
 
-// Define your event shape according to your usage
-interface LambdaEvent {
-[key: string]: any;
+function formatMessage(dates: moment.Moment[]){
+  const formattedDates = dates.map(date => (`\u2022 ${date.format('dddd, MMMM DD @ hh:mm a')}`)); // Unicode character for bullet point (•)
+  const msg = `New Global Entry appointments available on the following dates:\n${formattedDates.join('\n')}`;
+  return msg;  
 }
 
-interface LambdaResponse {
-    statusCode: number;
-    body: string;
-}
+async function notifyUsers(locationId: string, availableDates: moment.Moment[]) {
+  const client = await getConnectedClient();
+  const users: IUser[] = await getUsersWithLaterAppointmentQuery.run({ locationId, appointmentDate: availableDates[0].format('YYYY-MM-DD') }, client);
+  await client.end();
 
-interface Settings {
-    current_interview_date_str: string;
-    enrollment_location_id: string;
-}
-
-const settings: Settings = {
-    current_interview_date_str: "July 12, 2023",
-    enrollment_location_id: "5020"
-}
-
-async function getDates(settings: Settings): Promise<string[]> {
-    const dates: string[] = [];
-    try {
-        const goesURL: string = `https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=3&locationId=${settings.enrollment_location_id}&minimum=1`
-        const { data } = await axios.get(goesURL);
-
-        // Parse the json
-        if (!data) {
-            console.info('No tests available.');
-            return dates;
-        }
-
-        const currentApt = moment(settings.current_interview_date_str, 'MMMM DD, YYYY');
-
-
-        for (let o of data) {
-            if (o.active) {
-                const dtp = moment(o.startTimestamp, 'YYYY-MM-DDTHH:mm');
-
-                if (currentApt.isAfter(dtp)) {
-                    dates.push(dtp.format('dddd, MMMM DD @ hh:mm a'));
-                }
-            }
-        }
-        console.log(dates);
-        return dates;
-    } catch (error) {
-        console.error("Something went wrong when trying to obtain the openings:", error);
-        throw new Error("Failed to retrieve dates from the external API.");
+  for (let user of users) {
+    const userInterestedDates = availableDates.filter(date => date.isBefore(moment(user.currentAppointmentDate)));
+    if (userInterestedDates.length > 0) {
+      const msg = formatMessage(userInterestedDates);
+      await bot.telegram.sendMessage(user.userId, msg);
     }
-}
-
-function lambdaResponse(statusCode: number, message: string, event: LambdaEvent): LambdaResponse {
-    return {
-        statusCode,
-        body: JSON.stringify({
-            message,
-            input: event,
-        }),
-    };
-}
-
-// Generate the message text
-function formatMessage(dates: string[]){
-    const formattedDates = dates.map(date => (`\u2022 ${date}`)); // Unicode character for bullet point (•)
-    const msg = `New Global Entry appointments available on the following dates:\n${formattedDates.join('\n')}`;
-    return msg;  
+  }
 }
 
 export const checkAppointments = async (event: LambdaEvent): Promise<LambdaResponse> => {
-    try {
-        const dates = await getDates(settings);
-        if (!dates.length) {
-            return lambdaResponse(200, 'No new dates', event);
-        }
-        await bot.telegram.sendMessage(chatID, formatMessage(dates));
-        return lambdaResponse(200, 'User Notification Sent', event);
-    } catch (error) {
-        console.error("Error occurred while checking appointments:", error);
-        return lambdaResponse(500, error.message, event);
+  try {
+    const client = await getConnectedClient();
+    const locations: ILocation[] = await getLocationsQuery.run(undefined, client);
+    await client.end();
+
+    for (let location of locations) {
+      const earliestAvailableDates = await getDates(location.locationId);
+      if (earliestAvailableDates.length > 0) {
+        await notifyUsers(location.locationId, earliestAvailableDates);
+      }
     }
+    
+    return lambdaResponse(200, 'User Notification Sent', event);
+  } catch (error) {
+    console.error("Error occurred while checking appointments:", error);
+    return lambdaResponse(500, error.message, event);
+  }
 };
+
+function lambdaResponse(statusCode: number, message: string, event: LambdaEvent): LambdaResponse {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: message,
+      input: event,
+    }),
+  };
+}
+
+interface LambdaEvent {
+  [key: string]: any;
+}
+
+interface LambdaResponse {
+  statusCode: number;
+  body: string;
+}
--- src/queries.ts
+++ src/queries.ts
@@ -1,5 +1,3 @@
-// queries.ts
-
 import { sql } from '@pgtyped/runtime';
 
 export interface ISettingsInsertParams {
@@ -19,4 +17,37 @@ export const settingsInsertQuery = sql<ISettingsInsertQuery>`
     ON CONFLICT (userId) DO UPDATE
     SET locationId = excluded.locationId,
         currentAppointmentDate = excluded.currentAppointmentDate
+`;
+
+export interface ILocation {
+    locationId: string;
+}
+
+export interface IGetLocationsQuery {
+    params: void;
+    result: ILocation;
+}
+  
+export const getLocationsQuery = sql<IGetLocationsQuery>`
+    SELECT DISTINCT locationId FROM settings
+`;
+  
+export interface IGetUsersWithLaterAppointmentParams {
+    locationId: string;
+    appointmentDate: string;
+}
+
+export interface IUser { 
+    userId: string,
+    currentAppointmentDate: string,
+}
+
+export interface IGetUsersWithLaterAppointmentQuery {
+    params: IGetUsersWithLaterAppointmentParams;
+    result: IUser;
+}
+  
+export const getUsersWithLaterAppointmentQuery = sql<IGetUsersWithLaterAppointmentQuery>`
+    SELECT userId, currentAppointmentDate FROM settings
+    WHERE locationId = $locationId AND currentAppointmentDate > $appointmentDate
 `;
